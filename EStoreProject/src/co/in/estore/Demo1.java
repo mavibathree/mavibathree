2bac724fc7e2b63fa5a9760fcae597741c32e4d7
sys <sys@sys-PC>
2017-09-15 19:00:54 +0530

    @Vijay-I have created estore project and pushed into git

---
+++ EStoreProject/src/co/in/estore/Demo1.java
@@ -0,0 +1,5 @@
+package co.in.estore;
+
+public class Demo1 {   //this is single line comment and { is open braces
+//class body or scope or block
+}//} is close braces
---
+++ EStoreProject/src/co/in/estore/Demo2.java
@@ -0,0 +1,28 @@
+package co.in.estore;
+
+//below statements are java doc's
+
+/**
+ * 
+ * @author xyz
+ * @date
+ * @version 1.0
+ * @description
+ *
+ */
+
+public class Demo2 {
+	//alignment --->ctrl+shift+f
+	// we can define variables and methods inside the class
+
+	/**
+	 * @author sys
+	 * @param args
+	 */
+	// this is jvm method and entry point or execution engine
+	// shortcut key for main method type main-->ctrl+space bar
+	public static void main(String[] args) {
+
+	}
+
+}
